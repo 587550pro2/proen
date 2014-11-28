4e34190b6d612e73702263640b776072a2bc232d
ういうい <uiuitsukuba@users.noreply.github.com>
2014-11-28 16:31:39 +0900

    Update task_fri_1_2.rb

--- task_fri_1_2.rb
+++ task_fri_1_2.rb
@@ -1,9 +1,9 @@
 #Ensoding = Shift_JIS
 while
- str = gets
-	str.chomp!
-	if /(<TITLE>|<SUBTITLE>)/ =~ str
-		str.sub!(/.*/,"")
+ str = gets # 入力された文字列を読み込む
+	str.chomp! # 改行を削除
+	if /(<TITLE>|<SUBTITLE>)/ =~ str # 正規表現と照合
+		str.sub!(/.*/,"") # <TITLE>,<SUBTITLE>が含まれていれば文字列を削除する
 		#print str
 	else
 		print str
