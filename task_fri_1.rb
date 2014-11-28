df514042657fa882f2f6162645dc6dee62b368c5
uiui <uipr9264@gmail.com>
2014-11-28 16:11:01 +0900

    コメント挿入

--- task_fri_1.rb
+++ task_fri_1.rb
@@ -1,11 +1,11 @@
 #Ensoding = Shift_JIS
-while str = gets
-	str.chomp!
-	unless /(<TITLE>|<SUBTITLE>)/ =~ str
+while str = gets # 入力された文字列を読み込む
+	str.chomp! # 改行の削除
+	unless /(<TITLE>|<SUBTITLE>)/ =~ str # <TITLE>もしくは<SUBTITLE>が含まれているかどうかを判定する。
 		print (str)
 		print "\n"
 	else
-		str.delete("\w")
+		str.delete("\w") # 含まれていた場合、その文字列を削除する。 
 		print str
 		print "\n"
 	end
